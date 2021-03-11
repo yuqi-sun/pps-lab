f77f61d766d18acec3d4e5e93a038acc7ecda3d1
Yuqi Sun <yuqi.sun@studio.unibo.it>
2021-03-11 10:28:07 +0100

    [lab02] add shape ex. and its test

---
+++ lab02/src/lab/Shape.scala
@@ -0,0 +1,34 @@
+package lab
+
+sealed trait Shape
+
+object Shape {
+
+  case class Rectangle(base:Double, height:Double) extends Shape {
+    throwExceptionIfNegative(base)
+    throwExceptionIfNegative(height)
+  }
+  case class Circle(radius:Double) extends Shape {
+    throwExceptionIfNegative(radius)
+  }
+  case class Square(side:Double) extends Shape {
+    throwExceptionIfNegative(side)
+  }
+
+  def perimeter(shape:Shape): Double = shape match {
+    case Rectangle(base, height) => (base+height)*2
+    case Circle(radius) => radius*2*Math.PI
+    case Square(side) => side*4
+  }
+
+  def area(shape:Shape): Double = shape match {
+    case Rectangle(base, height) => base*height
+    case Circle(radius) => Math.pow(radius, 2)*Math.PI
+    case Square(side) => Math.pow(side, 2)
+  }
+
+  def throwExceptionIfNegative(n:Double): Unit = {
+    if (n < 0)
+      throw new IllegalArgumentException()
+  }
+}
---
+++ lab02/test/lab/ShapeTest.scala
@@ -0,0 +1,38 @@
+package lab
+
+import lab.Shape._
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class ShapeTest {
+
+  val side:Double = 1
+  val base:Double = 5
+  val height:Double = 3
+  val radius:Double = 3
+
+  @Test def testRectangle() {
+    val rectangle = Rectangle(base, height)
+    assertEquals((base+height)*2, perimeter(rectangle))
+    assertEquals(base*height, area(rectangle))
+  }
+
+  @Test def testCircle() {
+    val circle = Circle(radius)
+    assertEquals(2*radius*Math.PI, perimeter(circle))
+    assertEquals(Math.pow(radius, 2)*Math.PI, area(circle))
+  }
+
+  @Test def testSquare() {
+    val square = Square(side)
+    assertEquals(side*4, perimeter(square))
+    assertEquals(Math.pow(side, 2), area(square))
+  }
+
+  @Test def testNonPositiveShape(): Unit ={
+    assertThrows(classOf[IllegalArgumentException], () => Rectangle(-5, height))
+    assertThrows(classOf[IllegalArgumentException], () => Circle(-5))
+    assertThrows(classOf[IllegalArgumentException], () => Square(-5))
+  }
+
+}
