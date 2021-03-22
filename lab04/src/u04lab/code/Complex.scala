3dbada26ccc365b0d6de36801220fa417658716d
Yuqi Sun <yuqi.sun@studio.unibo.it>
2021-03-22 12:29:16 +0100

    [lab04] complete Complex object

--- lab04/src/u04lab/code/Complex.scala
+++ lab04/src/u04lab/code/Complex.scala
@@ -8,15 +8,40 @@ trait Complex {
 }
 
 object Complex {
-  def apply(re:Double, im:Double):Complex = ??? // Fill here
+  def apply(re:Double, im:Double):Complex = ComplexImpl(re, im)
+
+  private case class ComplexImpl(override val re:Double, override val im:Double) extends Complex {
+    override def +(c: Complex): Complex = ComplexImpl(re+c.re, im + c.im)
+
+    override def *(c: Complex): Complex = ComplexImpl(re*c.re - im*c.im, re*c.im + im*c.re)
+  }
+}
+
+class ComplexImpl(override val re:Double, override val im:Double) extends Complex {
+  override def +(c: Complex): Complex = new ComplexImpl(re+c.re, im + c.im)
+
+  override def *(c: Complex): Complex = new ComplexImpl(re*c.re - im*c.im, re*c.im + im*c.re)
 }
 
 object TryComplex extends App {
-  val a = Array(Complex(10,20), Complex(1,1), Complex(7,0))
-  val c = a(0) + a(1) + a(2)
-  println(c, c.re, c.im) // (ComplexImpl(18.0,21.0),18.0,21.0)
-  val c2 = a(0) * a(1)
-  println(c2, c2.re, c2.im) // (ComplexImpl(-10.0,30.0),-10.0,30.0)
+  //ComplexImpl class
+  var a = Array(new ComplexImpl(10,20), new ComplexImpl(1,1), new ComplexImpl(7,0))
+  var c = a(0) + a(1) + a(2)
+  var n = new ComplexImpl(10, 20)
+  println(c, c.re, c.im) //(u04lab.code.ComplexImpl@6a1aab78,18.0,21.0)
+  var c2 = a(0) * a(1)
+  println(c2, c2.re, c2.im) //(u04lab.code.ComplexImpl@8e24743,-10.0,30.0)
+  println(a(0) == n) //false
+
+
+  //case class
+  val b = Array(Complex(10,20), Complex(1,1), Complex(7,0))
+  val m = Complex(10, 20)
+  val c3 = b(0) + b(1) + b(2)
+  println(c3, c3.re, c3.im) //(ComplexImpl(18.0,21.0),18.0,21.0)
+  val c4 = b(0) * b(1)
+  println(c4, c4.re, c4.im) //(ComplexImpl(-10.0,30.0),-10.0,30.0)
+  println(b(0) == m) //true
 }
 
 /** Hints:
