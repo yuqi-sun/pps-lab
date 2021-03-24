6fc70f55caf8e7ea852626af09f4106315cb5d33
Yuqi Sun <yuqi.sun@studio.unibo.it>
2021-03-24 15:04:10 +0100

    [lab04] complete PowerIterators for ex.3

--- lab04/src/u04lab/code/PowerIterators.scala
+++ lab04/src/u04lab/code/PowerIterators.scala
@@ -1,7 +1,12 @@
 package u04lab.code
 
-import Optionals._
-import Lists._
+import Optionals.Option
+import Option.{Some, None}
+import Lists.List
+import Lists.List.{nil, append, foldRight, reverse}
+import Streams._
+import Stream._
+import scala.util.Random
 
 trait PowerIterator[A] {
   def next(): Option[A]
@@ -12,15 +17,38 @@ trait PowerIterator[A] {
 trait PowerIteratorsFactory {
 
   def incremental(start: Int, successive: Int => Int): PowerIterator[Int]
-  def fromList[A](list: List[A])
+  def fromList[A](list: List[A]): PowerIterator[A]
   def randomBooleans(size: Int): PowerIterator[Boolean]
 }
 
 class PowerIteratorsFactoryImpl extends PowerIteratorsFactory {
 
-  override def incremental(start: Int, successive: Int => Int): PowerIterator[Int] = ???
+  def toStream[A](list:List[A]): Stream[A] = foldRight(list)(empty[A]())(cons(_, _))
 
-  override def fromList[A](list: List[A]): Unit = ???
+  private case class PowerIteratorImpl[A](private var s:Stream[A]) extends PowerIterator[A] {
 
-  override def randomBooleans(size: Int): PowerIterator[Boolean] = ???
+    private var pastList: List[A] = nil
+
+    override def next(): Option[A] = s match {
+      case Cons(h, t) =>
+        val head = h()
+        pastList = append(pastList, List.Cons(head, nil))
+        s = t()
+        Some(head)
+      case _ => None()
+    }
+
+    override def allSoFar(): List[A] = pastList
+
+    override def reversed(): PowerIterator[A] = PowerIteratorImpl(toStream(reverse(pastList)))
+  }
+
+  override def incremental(start: Int, successive: Int => Int): PowerIterator[Int] =
+    new PowerIteratorImpl[Int](iterate(start)(successive))
+
+  override def fromList[A](list: List[A]): PowerIterator[A] =
+    new PowerIteratorImpl[A](toStream(list))
+
+  override def randomBooleans(size: Int): PowerIterator[Boolean] =
+    new PowerIteratorImpl[Boolean](take(generate(Random.nextBoolean()))(size))
 }
--- lab04/test/u04lab/code/PowerIteratorsTest.scala
+++ lab04/test/u04lab/code/PowerIteratorsTest.scala
@@ -10,17 +10,72 @@ class PowerIteratorsTest {
 
   val factory = new PowerIteratorsFactoryImpl()
 
-  @Test
-  def testIncremental() {
-    val pi = factory.incremental(5,_+2); // pi produce 5,7,9,11,13,...
-    assertEquals(Option.of(5), pi.next());
-    assertEquals(Option.of(7), pi.next());
-    assertEquals(Option.of(9), pi.next());
-    assertEquals(Option.of(11), pi.next());
-    assertEquals(List.Cons(5, List.Cons(7, List.Cons(9, List.Cons(11,List.Nil())))), pi.allSoFar()); // elementi già prodotti
-    for (i <- 0 until 10) {
-      pi.next(); // procedo in avanti per un po'..
+  @Test def testIncremental() {
+    val pi = factory.incremental(5,_+2); //pi produce 5,7,9,11,13,...
+    assertEquals(Option.of(5), pi.next())
+    assertEquals(Option.of(7), pi.next())
+    assertEquals(Option.of(9), pi.next())
+    assertEquals(Option.of(11), pi.next())
+    assertEquals(Cons(5, Cons(7, Cons(9, Cons(11, nil)))), pi.allSoFar()); //elementi già prodotti
+    for (_ <- 0 until 10) {
+      pi.next(); //procedo in avanti per un po'..
     }
-    assertEquals(Option.of(33), pi.next()); // sono arrivato a 33
+    assertEquals(Option.of(33), pi.next()); //sono arrivato a 33
+  }
+
+  @Test def testRandom(): Unit = { //semi-automatico, si controlleranno anche le stampe a video
+    val pi = factory.randomBooleans(4) //pi produce 4 booleani random
+    val b1 = pi.next()
+    val b2 = pi.next()
+    val b3 = pi.next()
+    val b4 = pi.next()
+    System.out.println(b1 + " " + b2 + " " + b3 + " " + b4) //stampo a video.. giusto per vedere se sono proprio random..
+
+    assertTrue(Option.isEmpty(pi.next())) //ne ho già prodotti 4, quindi il prossimo è un opzionale vuoto
+
+    assertEquals(Cons(b1, Cons(b2, Cons(b3, Cons(b4, nil)))), map(pi.allSoFar())(Option.Some(_))) // ho prodotto proprio b1,b2,b3,b4
+  }
+
+  @Test def testFromList(): Unit = {
+    val pi = factory.fromList(Cons("a", Cons("b", Cons("c", nil)))) // pi produce a,b,c
+    assertEquals(Option.of("a"), pi.next())
+    assertEquals(Option.of("b"), pi.next())
+    assertEquals(Cons("a", Cons("b", nil)), pi.allSoFar()) // fin qui a,b
+
+    assertEquals(Option.of("c"), pi.next())
+    assertEquals(Cons("a", Cons("b", Cons("c", nil))), pi.allSoFar()) // fin qui a,b,c
+
+    assertTrue(Option.isEmpty(pi.next())) // non c'è più niente da produrre
+
+  }
+
+  @Test def optionalTestReversedOnList(): Unit = {
+    val pi = factory.fromList(Cons("a", Cons("b", Cons("c", nil))))
+    assertEquals(Option.of("a"), pi.next())
+    assertEquals(Option.of("b"), pi.next())
+    val pi2 = pi.reversed() //pi2 itera su b,a
+    assertEquals(Option.of("c"), pi.next()) // c viene prodotto da pi normalmente
+
+    assertTrue(Option.isEmpty(pi.next()))
+    assertEquals(Option.of("b"), pi2.next())
+    assertEquals(Option.of("a"), pi2.next())
+    assertEquals(Cons("b", Cons("a", nil)), pi2.allSoFar()) // pi2 ha prodotto b,a
+
+    assertTrue(Option.isEmpty(pi2.next()))
+  }
+
+  @Test def optionalTestReversedOnIncremental(): Unit = {
+    val pi = factory.incremental(0, x => x + 1) // 0,1,2,3,...
+    assertEquals(Option.of(0), pi.next())
+    assertEquals(Option.of(1), pi.next())
+    assertEquals(Option.of(2), pi.next())
+    assertEquals(Option.of(3), pi.next())
+    val pi2 = pi.reversed() // pi2 itera su 3,2,1,0
+    assertEquals(Option.of(3), pi2.next())
+    assertEquals(Option.of(2), pi2.next())
+    val pi3 = pi2.reversed() // pi2 ha prodotto 3,2 in passato, quindi pi3 itera su 2,3
+    assertEquals(Option.of(2), pi3.next())
+    assertEquals(Option.of(3), pi3.next())
+    assertTrue(Option.isEmpty(pi3.next()))
   }
 }
